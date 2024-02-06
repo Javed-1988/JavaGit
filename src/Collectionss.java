54ae31090c9d6432360a210b720d28c82828f68d
javed <mahmoodzahedi1988@gmail.com>
2024-02-06 16:38:46 +0530

    commit changes to local git Collections.java

---
+++ src/Collectionss.java
@@ -0,0 +1,17 @@
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Collectionss {
+
+    public static void man(String[]args)
+    {
+        ConcurrentHashMap<Integer,String> con = new ConcurrentHashMap<>();
+        con.put(1,"jav");
+        con.put(2,"jak");
+        con.put(3,"jam");
+
+        for (Map.Entry<Integer, String> entry : con.entrySet()) {
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
+    }
+}
